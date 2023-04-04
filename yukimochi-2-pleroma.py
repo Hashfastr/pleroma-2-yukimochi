419c16c7972d1eb1de921099ef3051b87a1ec7a8
sjones <robotsguy@gmail.com>
2023-04-04 09:34:34 -0600

    Added yuki2pleroma conversion

---
+++ yukimochi-2-pleroma.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+# Pleroma
+#"relay-list": {
+#        "eozygodon.com": {
+#            "domain": "eozygodon.com",
+#            "inbox": "https://eozygodon.com/inbox",
+#            "followid": "https://eozygodon.com/4dc0e1d2-cc85-4271-94c2-e96ae2420fc6",
+#            "software": "mastodon"
+#        },
+
+# Yuki
+# {
+#   "domain":"publicsquare.global",
+#   "inbox_url":"https://publicsquare.global/inbox",
+#   "activity_id":"https://publicsquare.global/63a83b79-22bd-44de-b1c0-bf88fc82285f",
+#   "actor_id":"https://publicsquare.global/actor"
+# }
+
+import json
+
+new_data = dict()
+new_data['relay-list'] = dict()
+subs = new_data['relay-list']
+
+with open('backup.json', 'r') as f:
+    data = json.load(f)
+
+    for i in data["subscriptions"]:
+        subs[i['domain']] = dict()
+        subs[i['domain']]['domain'] = i['domain']
+        subs[i['domain']]['inbox'] = i['inbox_url']
+        subs[i['domain']]['followid'] = i['activity_id']
+        subs[i['domain']]['software'] = "unknown"
+
+    new_data['private-key'] = ''
+    new_data['follow-requests'] = dict()
+    new_data['version'] = 1
+    with open('actor.pem', 'r') as key:
+        for line in key:
+            new_data['private-key'] = new_data['private-key'] + line
+
+    with open('new-pleroma.json', 'w') as f2:
+        f2.write(json.dumps(new_data, indent=4))
